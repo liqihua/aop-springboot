1302bd13770e9a0ec337cf21f63ed4517d50ba5d
liqihua <iamliqihua@qq.com>
2018-11-30 11:43:01 +0800

    liqihua

--- src/main/java/com/liqihua/aop/AopConfig.java
+++ src/main/java/com/liqihua/aop/AopConfig.java
@@ -1,16 +1,27 @@
 package com.liqihua.aop;
 
 
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSON;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +32,7 @@ import java.util.Map;
 @Aspect
 @Configuration
 public class AopConfig {
+    private static final Logger log = LoggerFactory.getLogger(AopConfig.class);
 
 
 
@@ -97,6 +109,78 @@ public class AopConfig {
     }
 
 
+    /**
+     * 环绕日志以及参数处理
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
+    @Around(value = "execution (* com.liqihua.controller.*.*(..))")
+    public Object controllerAround(ProceedingJoinPoint joinPoint) throws Throwable{
+        /**
+         * 请求日志打印
+         */
+        String classAndMethodName = null;
+        //获取当前请求属性集
+        ServletRequestAttributes sra = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        //获取请求
+        HttpServletRequest request = sra.getRequest();
+        //获取请求地址
+        String requestUrl = request.getRequestURL().toString();
+        //记录请求地址
+        log.info("请求路径[{}]", requestUrl);
+        //记录请求开始时间
+        long startTime = System.currentTimeMillis();
+        try {
+            Class<?> target = joinPoint.getTarget().getClass();
+            MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+            Class<?>[] paramTypes = methodSignature.getParameterTypes();
+            String methodName = joinPoint.getSignature().getName();
+            //获取当前执行方法
+            Method currentMethod = target.getMethod(methodName, paramTypes);
+            //拼接输出字符串
+            classAndMethodName = target.getName() + " 的 " + currentMethod.getName() + " 方法";
+            log.info("正在执行：{}", classAndMethodName);
+            //打印参数
+            Enumeration<String> enumeration = request.getParameterNames();
+            while (enumeration.hasMoreElements()){
+                String param = enumeration.nextElement();
+                log.info("参数 " + param+" : "+request.getParameter(param));
+            }
 
+            /**
+             * RequestParam注解参数非空参数拦截
+             */
+            Parameter[] params = currentMethod.getParameters();
+            if(params != null && params.length > 0){
+                Object[] args = joinPoint.getArgs();
+                for(int i=0; i< params.length; i++){
+                    Annotation[] annos = params[i].getDeclaredAnnotations();
+                    if(annos != null && annos.length > 0){
+                        for(Annotation anno : annos){
+                            if(anno instanceof RequestParam){
+                                RequestParam annoObj = (RequestParam)anno;
+                                if(annoObj.required()){
+                                    if(args[i] == null || (args[i] instanceof String && StrUtil.isBlank(args[i]+""))){
+                                        String paramName = methodSignature.getParameterNames()[i];
+                                        String msg = "参数 "+paramName+" 要求非空，参数值："+(args[i]==null?"":args[i].toString());
+                                        log.info(msg);
+                                        return msg;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Throwable e) {
+            log.error("controllerAround 发生异常:", e);
+        }
+        Object object = joinPoint.proceed();
+        log.info("返回: {}", object==null?"空": JSON.toJSONString(object));
+        long endTime = System.currentTimeMillis();
+        log.info("响应时间 {} 毫秒", endTime-startTime);
+        return object;
+    }
 
 }
